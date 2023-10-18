55f633229e0a71b71b777a1bc43cf0552916ff38
Joshua Stanford <stanjosh@gmail.com>
2023-10-18 18:48:42 -0500

    moved "@vitejs/plugin-react": "^4.1.0",  to deps

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // {
-    //   name: 'treat-js-files-as-jsx',
-    //   async transform(code, id) {
-    //     if (!id.match(/src\/.*\.js$/))  return null
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/))  return null
 
-    //     // Use the exposed transform from vite, instead of directly
-    //     // transforming with esbuild
-    //     return transformWithEsbuild(code, id, {
-    //       loader: 'jsx',
-    //       jsx: 'automatic',
-    //     })
-    //   },
-    // },
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },
     react(),
   ],
 
