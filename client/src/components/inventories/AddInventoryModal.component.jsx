843a9f3971d624a8db4f658dfe2302d814f89e99
Joshua Stanford <stanjosh@gmail.com>
2023-10-18 18:51:23 -0500

    remove auth from unneccessary places

--- client/src/components/inventories/AddInventoryModal.component.jsx
+++ client/src/components/inventories/AddInventoryModal.component.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { CREATE_INVENTORY } from '../../util/mutations';
-import Auth from '../../util/auth';
+
 
 
 const AddInventoryForm = () => {
