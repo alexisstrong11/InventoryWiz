0cfa3e2e8528c5fa118fa5ad6ecfdd1d3c613867
Joshua Stanford <stanjosh@gmail.com>
2023-10-18 18:00:52 -0500

    fixed minor style removed extra fields and buttons

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Container } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_PRODUCT_TO_INVENTORY } from '../util/mutations'; 
 
 function AddProduct({inventoryId}) {
   const [upc, setUpc] = useState('');
-  const [brand, setBrand] = useState('');
+
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
-  const [quantity, setQuantity] = useState(''); // New state for Quantity
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
 
@@ -28,7 +27,6 @@ function AddProduct({inventoryId}) {
           inventoryId : inventoryId,
           productInput: { 
             UPC: upc,
-            brand : brand,
             category : category,
             description : description,
             name : name,
@@ -40,16 +38,14 @@ function AddProduct({inventoryId}) {
 
       if (data) {
         // Product saved successfully
-        s
+        setShowErrorMessage(false);
         setShowSuccessMessage(true);
         // Reset the form
         setUpc('');
-        setBrand('');
         setCategory('');
         setDescription('');
         setName('');
         setPrice('');
-        setQuantity(''); // Reset the Quantity field
       }
     } catch (error) {
       // Handle any errors, such as displaying an error message
@@ -58,24 +54,33 @@ function AddProduct({inventoryId}) {
   };
 
   return (
-    <div>
+    <Container>
       <h2>Add Product</h2>
       <Form onSubmit={handleSubmit}>
+      <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </Form.Group>
+
         <Form.Group>
           <Form.Label>UPC</Form.Label>
           <Form.Control
             type="text"
             value={upc}
             onChange={(e) => setUpc(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Brand</Form.Label>
-          <Form.Control
-            type="text"
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)}
           />
         </Form.Group>
 
@@ -97,33 +102,6 @@ function AddProduct({inventoryId}) {
           />
         </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            type="number"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Quantity</Form.Label>
-          <Form.Control
-            type="number"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-          />
-        </Form.Group>
-
         <Button type="submit">Add Product</Button>
       </Form>
 
@@ -134,7 +112,7 @@ function AddProduct({inventoryId}) {
       {showErrorMessage && (
         <Alert variant="danger">Error adding product. Please try again.</Alert>
       )}
-    </div>
+    </Container>
   );
 }
 
