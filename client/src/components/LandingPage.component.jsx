53485d829e2234555e76afd7a3286b1b6837cfaa
Joshua Stanford <stanjosh@gmail.com>
2023-10-19 17:30:22 -0500

    landing page box style

--- client/src/components/LandingPage.component.jsx
+++ client/src/components/LandingPage.component.jsx
@@ -8,9 +8,8 @@ const LandingPage = () => {
             <div className="landingPageBox">
                 <h1>Welcome to InventoryWiz!</h1>
 
-                <h3>Experience the power of InventoryWiz: </h3>
-                <h3>the ultimate solution for managing your pantry effortlessly. </h3>
-                <h3>Sign up now to start optimizing your inventory management!</h3>
+                <h4>A perfect tool for managing your pantry effortlessly. </h4>
+                <h4>Sign up now to start optimizing your inventory management!</h4>
                 
                <SignUpForm/>  
                
