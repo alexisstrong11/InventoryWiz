e75e50bc32c23ab1de47f02bc6e46e111b67a5a5
Evelyn Jimenez <evey.plt@gmail.com>
2023-10-18 19:13:08 -0500

    added Landing Page Welcome text and CSS

--- client/src/components/LandingPage.component.jsx
+++ client/src/components/LandingPage.component.jsx
@@ -7,6 +7,10 @@ const LandingPage = () => {
       <>
             <div class = "landingPageBox">
                 <h1>Welcome to InventoryWiz!</h1>
+
+                <h3>Experience the power of InventoryWiz: </h3>
+                <h3>the ultimate solution for managing your pantry effortlessly. </h3>
+                <h3>Sign up now to start optimizing your inventory management!</h3>
                 
                <SignUpForm/>  
                
